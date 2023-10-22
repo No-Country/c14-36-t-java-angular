010546baf45a23671daa7a1c3d1aa0113eeeda72
Rpeche-pk <pecheaparcana1998@gmail.com>
2023-10-22 16:36:51 -0500

    feature(AuthController): 🚀 small changes

--- cashier-backend/src/main/java/com/nocountry/cashier/controller/rest/AuthController.java
+++ cashier-backend/src/main/java/com/nocountry/cashier/controller/rest/AuthController.java
@@ -9,20 +9,14 @@ import com.nocountry.cashier.controller.dto.response.GenericResponseDTO;
 import com.nocountry.cashier.domain.usecase.AuthService;
 import com.nocountry.cashier.domain.usecase.UserService;
 import com.nocountry.cashier.domain.usecase.qr.QRGeneratorService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Locale;
 import java.util.Map;
 
 import static com.nocountry.cashier.util.Constant.API_VERSION;
@@ -42,19 +36,20 @@ public class AuthController {
     private final AuthService authService;
     private final UserService userService;
     private final QRGeneratorService qrService;
+
     @PostMapping("/")
     public ResponseEntity<?> registerCustomer(@Valid @RequestBody UserRequestDTO authRequestDTO) {
-        String urlConfirm = ServletUriComponentsBuilder.fromCurrentRequest().path("{path}").buildAndExpand("confirm").toUriString()+"?token=";
-        AuthResponseDTO register = authService.register(authRequestDTO,urlConfirm);
-        return ResponseEntity.ok().body(Map.of("data",register));
+        String urlConfirm = ServletUriComponentsBuilder.fromCurrentRequest().path("{path}").buildAndExpand("confirm").toUriString() + "?token=";
+        AuthResponseDTO register = authService.register(authRequestDTO, urlConfirm);
+        return ResponseEntity.ok().body(Map.of("data", register));
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> authenticateCustomer(@Valid @RequestBody AuthRequestDTO authDto){
+    public ResponseEntity<?> authenticateCustomer(@Valid @RequestBody AuthRequestDTO authDto) {
         AuthenticatedUserDTO authenticate = authService.authenticate(authDto);
         String uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/")
                 .path("{id}").buildAndExpand(authenticate.getId()).toUriString();
-        return ResponseEntity.ok().body(Map.of("data",authenticate,"url",uri));
+        return ResponseEntity.ok().body(Map.of("data", authenticate, "url", uri));
     }
 
 
@@ -63,17 +58,17 @@ public class AuthController {
         AuthConfirmedDTO confirmed = authService.confirm(token);
         String uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/")
                 .path("{qr}").path("view").buildAndExpand(confirmed.getQr()).toUriString();
-        return ResponseEntity.ok().body(Map.of("user",confirmed,"urlQr",uri));
+        return ResponseEntity.ok().body(Map.of("data", confirmed, "urlQr", uri));
     }
 
     // ? OJO CON ESTO SE MOVE TODAS LAS FUNCIONES A USER CONTROLLER
     @GetMapping("/{uuid}/view")
-    public ResponseEntity<?> findCustomerById(@PathVariable @NotBlank(message = "No puede ser vacío") String uuid){
-        return ResponseEntity.ok(new GenericResponseDTO<>(true,"Usuario Encontrado",userService.getById(uuid).get()));
+    public ResponseEntity<?> findCustomerById(@PathVariable @NotBlank(message = "No puede ser vacío") String uuid) {
+        return ResponseEntity.ok(new GenericResponseDTO<>(true, "Usuario Encontrado", userService.getById(uuid).get()));
     }
 
     @GetMapping("/confirm/{qr}")
-    public ResponseEntity<?> showQrCustomer(@PathVariable String qr){
+    public ResponseEntity<?> showQrCustomer(@PathVariable String qr) {
         return qrService.uploadLocalImage(qr);
     }
 }
