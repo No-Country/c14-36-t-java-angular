31d04e3795e2b07920519053764419c684661c66
Rpeche-pk <pecheaparcana1998@gmail.com>
2023-10-22 23:41:52 -0500

    refactor(backend): 🔧changes completed imports

--- cashier-backend/src/main/java/com/nocountry/cashier/controller/rest/TransactionController.java
+++ cashier-backend/src/main/java/com/nocountry/cashier/controller/rest/TransactionController.java
@@ -4,12 +4,9 @@ import com.nocountry.cashier.controller.dto.request.PageableDto;
 import com.nocountry.cashier.controller.dto.request.TransactionRequestDTO;
 import com.nocountry.cashier.controller.dto.response.GenericResponseDTO;
 import com.nocountry.cashier.controller.dto.response.TransactionResponseDTO;
-import com.nocountry.cashier.enums.EnumsState;
-
-import com.nocountry.cashier.enums.EnumsTransactions;
-import com.nocountry.cashier.exception.RegisterNotFound;
-
 import com.nocountry.cashier.domain.usecase.TransactionService;
+import com.nocountry.cashier.enums.EnumsState;
+import com.nocountry.cashier.enums.EnumsTransactions;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,6 +31,7 @@ public class TransactionController {
     private static final Logger logger = LoggerFactory.getLogger(TransactionController.class);
     @Autowired
     private TransactionService transactionService;
+
     //GetALLTransactions
     //http://localhost:8080/v1/api/customers/transactions?page=0&size=4&order=1&field=id
     @GetMapping
@@ -52,18 +50,20 @@ public class TransactionController {
     //NewTransaction
     //http://localhost:8080/v1/api/customers/transactions/new/b4c5d80d-1e38-4b63-bdb2-aa355973c07c
     @PostMapping("/new/{idAccount}")
-    public ResponseEntity<?> createTransaction(@RequestBody TransactionRequestDTO requestDTO, @PathVariable String idAccount){
-        TransactionResponseDTO transactionResponse = transactionService.createTransaction(requestDTO,idAccount);
+    public ResponseEntity<?> createTransaction(@RequestBody TransactionRequestDTO requestDTO, @PathVariable String idAccount) {
+        TransactionResponseDTO transactionResponse = transactionService.createTransaction(requestDTO, idAccount);
 //        String uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("")
 //                .path("{id}").buildAndExpand(transactionResponse.id()).toUriString();
         return ResponseEntity.status(CREATED).body(transactionResponse.toString());
     }
+
     //SearchById
     //http://localhost:8080/v1/api/customers/transactions/search/58c6f82a-57f0-4b74-ba56-2dfcd6665a54
     @GetMapping("/search/{id}")
     public ResponseEntity<?> getTransactionById(@PathVariable String id) {
         return ResponseEntity.ok(new GenericResponseDTO<>(true, "Transaccion Encontrada", transactionService.getById(id).get()));
     }
+
     //SearchByState
     //http://localhost:8080/v1/api/customers/transactions/search/state?idAccount=a37cf234-fad2-44f0-95e2-e17a532939f4&state=DONE&page=0&size=4&order=1&field=id
     @GetMapping("/search/state")
@@ -76,7 +76,7 @@ public class TransactionController {
         pageableDto.setSize(size);
 
         try {
-            List<TransactionResponseDTO> content = transactionService.findByState(state, idAccount,pageableDto).getContent();
+            List<TransactionResponseDTO> content = transactionService.findByState(state, idAccount, pageableDto).getContent();
             Map<String, Object> response = Map.of("message", "Listado Por Estado De Transaccion", "data", content);
             return new ResponseEntity<>(response, OK);
         } catch (Exception e) {
@@ -90,17 +90,17 @@ public class TransactionController {
     //http://localhost:8080/v1/api/customers/transactions/search/type?idAccount=a37cf234-fad2-44f0-95e2-e17a532939f4&type=DEPOSIT&page=0&size=4&order=1&field=id
     @GetMapping("/search/type")
     public ResponseEntity<?> getTransactionsByType(@RequestParam String idAccount,
-    @RequestParam EnumsTransactions type,
-    @RequestParam(value = "page", defaultValue = "0") Integer page,
-    @RequestParam(value = "size", defaultValue = "4") Integer size,
-    PageableDto pageableDto) {
+                                                   @RequestParam EnumsTransactions type,
+                                                   @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                   @RequestParam(value = "size", defaultValue = "4") Integer size,
+                                                   PageableDto pageableDto) {
 
 
         pageableDto.setPage(page);
         pageableDto.setSize(size);
 
         try {
-            List<TransactionResponseDTO> content = transactionService.findByType(type, idAccount,pageableDto).getContent();
+            List<TransactionResponseDTO> content = transactionService.findByType(type, idAccount, pageableDto).getContent();
             Map<String, Object> response = Map.of("message", "Listado Por Tipo De Transacccion", "data", content);
             return new ResponseEntity<>(response, OK);
         } catch (Exception e) {
@@ -108,6 +108,7 @@ public class TransactionController {
         }
 
     }
+
     //SearchByAmount
     //http://localhost:8080/v1/api/customers/transactions/search/amount?idAccount=a37cf234-fad2-44f0-95e2-e17a532939f4&amount=900&page=0&size=4&order=1&field=id
     @GetMapping("/search/amount")
@@ -118,7 +119,7 @@ public class TransactionController {
                                                      PageableDto pageableDto) {
 
         try {
-            List<TransactionResponseDTO> content = transactionService.findByAmount(amount ,idAccount,pageableDto).getContent();
+            List<TransactionResponseDTO> content = transactionService.findByAmount(amount, idAccount, pageableDto).getContent();
             Map<String, Object> response = Map.of("message", "Listado Por De Transacciones Por Monto", "data", content);
             return new ResponseEntity<>(response, OK);
         } catch (Exception e) {
