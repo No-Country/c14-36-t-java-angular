367b4accd53e2d6f04eba8f70afcd9755d9c3101
Raul1978x <108808330+Raul1978x@users.noreply.github.com>
2023-10-25 12:15:30 -0300

    fix: frontend-backend2

---
+++ cashier-app/src/app/component/Pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { CardComponent } from '../card/card.component'
+import {  CreditCardComponent } from '../credit-card/credit-card.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+}
