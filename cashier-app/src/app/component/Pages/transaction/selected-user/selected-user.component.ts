53189281078cc11e68861103a1c1faa8c9386e1e
Dexametasona <machicadogomezalexander@gmail.com>
2023-10-28 12:00:14 -0500

    feat: temporary update 2

--- cashier-app/src/app/component/Pages/transaction/selected-user/selected-user.component.ts
+++ cashier-app/src/app/component/Pages/transaction/selected-user/selected-user.component.ts
@@ -3,8 +3,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { enterLateral, fadeAnimation } from 'src/app/animations/animation';
 import { IUserTarget } from 'src/app/interfaces/User.interface';
 import { IAccount } from 'src/app/interfaces/account.interface';
+import { ITransactionDTO } from 'src/app/interfaces/transaction.interface';
 import { transactionView } from 'src/app/interfaces/transactionView.interface';
 import { AccountService } from 'src/app/services/account.service';
+import { TransferService } from 'src/app/services/transfer.service';
 
 @Component({
   selector: 'app-selected-user',
@@ -21,7 +23,8 @@ export class SelectedUserComponent {
   userTargetAccount!:IAccount;
 
   constructor(
-    private fb:FormBuilder
+    private fb:FormBuilder,
+    private transferServ:TransferService
   ){
     this.initTransferForm();
   }
@@ -42,7 +45,22 @@ export class SelectedUserComponent {
     })
   }
   onTransferSubmit(){
-
+    const data = this.transferForm.value as {issue:string, amount:number}
+    const dataDTO= {
+      amount:50,
+      state:"DONE",
+      type:"DEPOSIT"
+    }
+    console.log(dataDTO);
+    const {idAccount} = this.userTarget;
+    console.log(idAccount)
+    this.transferServ.newTransfer(idAccount,dataDTO).subscribe({
+      next:(res)=>{
+        console.log(res)
+        this.updateAlertStatus();
+      },
+      error(err){console.log(err)}
+    })
   }
 
 }
