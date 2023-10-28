ca31c1150ce30ab73eea8caa9015b3646db0424d
Ra <108808330+Raul1978x@users.noreply.github.com>
2023-10-28 10:59:27 -0300

    Update environment.ts

--- cashier-app/src/environments/environment.ts
+++ cashier-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    api: "http://localhost:9698/v1/api/login/",
-    api2:"http://localhost:9698/v1/api/customers?page=0&size=4&order=1&field=id"
+    api: "http://localhost:8080/v1/api/login/",
+    api2:"http://localhost:8080/v1/api/customers?page=0&size=4&order=1&field=id"
 }
