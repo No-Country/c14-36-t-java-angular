4defb5c715befd38cb6800e728f21190233da739
Jorge Ricardo <jandricardov@gmail.com>
2023-10-30 16:55:27 -0500

    perf: carga de consumo de solicitudes con promesas en dashboard y metricas

--- cashier-app/src/app/component/Pages/dashboard/dashboard.component.ts
+++ cashier-app/src/app/component/Pages/dashboard/dashboard.component.ts
@@ -26,7 +26,117 @@ export class DashboardComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Mostrar el indicador de carga.
+    this.isLoading = true;
+    const userDataDecoded = this.tokenService.getTokenDecoded();
+    const idUser = userDataDecoded.id;
+    console.log('idUser: ', idUser);
+    console.log('token Decoded: ', userDataDecoded);
+
+    const obtenerDatos = async () => {
+      try {
+        await this.obtenerDatosPorUUID(idUser);
+        await this.obtenerTransaccionesPorAccountId();
+        await this.obtenerDatosCuentaPorAccountId(this.idAccount);
+        this.isLoading = false;
+      } catch (error) {
+        console.error('Error:', error);
+        this.isLoading = false;
+      }
+    };
+
+    obtenerDatos();
+  }
+
+  /**
+   * The function `obtenerDatosPorUUID` is an asynchronous function that takes a UUID as a parameter
+   * and returns a Promise that resolves with void.
+   * @param {string} uuid - A string representing the unique identifier of a user.
+   * @returns The function `obtenerDatosPorUUID` returns a Promise of type `void`.
+   */
+  async obtenerDatosPorUUID(uuid: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.dashboardService.getUserDataById(uuid).subscribe(
+        (response) => {
+          if (response.success && response.message === 'Usuario Encontrado') {
+            console.log('Datos Usuario0 : ', response.data);
+            const userData = response.data;
+            this.idAccount = userData.idAccount;
+            resolve();
+          } else {
+            console.log('Error', 'No se encontró al usuario', 'error');
+            reject('No se encontró al usuario'); 
+          }
+        },
+        (error) => {
+          console.error('Error al obtener datos de usuario:', error);
+          reject(error); 
+        }
+      );
+    });
+  }
+
+  
+  /**
+   * The function obtains transactions by account ID and sorts them by date.
+   * @returns a Promise of type void.
+   */
+  async obtenerTransaccionesPorAccountId(): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+      try {
+        const response = await this.dashboardService.getTransactionsByAccountId(
+          this.idAccount
+        );
+        console.log('Transacciones: ', response);
+
+        if (response.data && response.data.length > 0) {
+          this.transacciones = Object.values(response.data);
+          this.transacciones = response.data.sort((a: any, b: any) => {
+            return (
+              new Date(b.dateEmit).getTime() - new Date(a.dateEmit).getTime()
+            );
+          });
+          resolve(); 
+        } else {
+          console.log('Información', 'No hay transacciones', 'info');
+          reject('No hay transacciones');
+        }
+      } catch (error) {
+        console.error('Error al obtener las transacciones', error);
+        reject(error);
+      }
+    });
+  }
+
+  
+  /**
+   * The function "obtenerDatosCuentaPorAccountId" retrieves account data by account ID and assigns the
+   * total account balance and CVU to class variables.
+   * @param {string} idAccount - The id of the account for which you want to obtain the data.
+   * @returns a Promise that resolves to void.
+   */
+  async obtenerDatosCuentaPorAccountId(idAccount: string): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+      try {
+        const response = await this.dashboardService.getAccountDataById(
+          idAccount
+        );
+        console.log('Data de la cuenta:', response);
+        this.saldo = response.totalAccount;
+        console.log('Data de la cuenta:', this.saldo);
+        this.cvu = response.cvu;
+        this.showcvu = true;
+        resolve(); 
+      } catch (error) {
+        console.error('Error al obtener los datos de la cuenta', error);
+        reject(error);
+      }
+    });
+  }
+
+  /*
+
+    ON INIT ANTIGUO
+    //Mostrar el indicador de carga.
     this.isLoading = true;
     const userDataDecoded = this.tokenService.getTokenDecoded();
     const idUser = userDataDecoded.id;
@@ -41,13 +151,13 @@ export class DashboardComponent implements OnInit {
     setTimeout(() => {
       this.obtenerDatosCuentaPorAccountId(this.idAccount);
     }, 1000);
-    // Mostrar el indicador de carga.
+    //Mostrar el indicador de carga.
     
     setTimeout(() => {
       this.isLoading= false;
     }, 1000);
-  }
 
+    FUNCIONALIDAD ANTIGUA
   obtenerDatosPorUUID(uuid: string) {
     this.dashboardService.getUserDataById(uuid).subscribe(
       (response) => {
@@ -104,48 +214,7 @@ export class DashboardComponent implements OnInit {
     }
   }
 
-  /*
-    obtenerTransaccionesPorAccountId() {
-    this.dashboardService.getTransactionsByAccountId(this.idAccount).subscribe(
-      (response) => {
-        console.log('soy la data de listado de transacciones: ', response);
-        if (response.data && response.data.length > 0) {
-          this.transacciones = Object.values(response.data);
-          this.transacciones = response.data.sort((a: any, b: any) => {
-            return (
-              new Date(b.dateEmit).getTime() - new Date(a.dateEmit).getTime()
-            );
-          });
-          console.log('Transacciones:', this.transacciones);
-        } else {
-          console.log('Información', 'No hay transacciones', 'info');
-        }
-      },
-      (error) => {
-        console.log(
-          'Error',
-          'No se pudieron obtener las transacciones',
-          'error'
-        );
-      }
-    );
-  }
 */
-
-  /*
-  obtenerDatosCuentaPorAccountId(idAccount: string) {
-    this.dashboardService.getAccountDataById(idAccount).subscribe(
-      (response) => {
-        console.log('Soy la data:', response);
-        this.saldo = response.totalAccount;
-        this.cvu = response.cvu;
-        console.log('Soy el saldo:', this.saldo, 'soy el cvu:', this.cvu);
-      },
-      (error) => {
-        console.error('Error al obtener los datos de la cuenta', error);
-      }
-    );
-  }*/
 
   showCvu(): void {
     this.dashboardService.showCvu();
--- cashier-app/src/app/component/Pages/dashboard/metricas/metricas.component.ts
+++ cashier-app/src/app/component/Pages/dashboard/metricas/metricas.component.ts
@@ -28,42 +28,63 @@ export class MetricasComponent implements OnInit{
               private tokenService: TokenService) {}
 
   ngOnInit(): void {
-     this.isLoading = true;
-     const userDataDecoded = this.tokenService.getTokenDecoded();
-     let idUser:any = userDataDecoded?.id;
-     this.obtenerDatosPorUUID(idUser);
-     setTimeout(() => {
-       this.obtenerTransaccionesPorAccountId();
-     }, 1000);
-     setTimeout(() => {
-       this.obtenerDatosCuentaPorAccountId(this.idAccount);
-       this.showcvu = true;
-     }, 1000);
-     this.parsearData()
-     setTimeout(() => {
-      this.isLoading= false;
-    }, 2000);
-     
-  }
+    this.isLoading = true;
+    const userDataDecoded = this.tokenService.getTokenDecoded();
+    const idUser = userDataDecoded.id;
+    console.log('idUser: ', idUser);
+    console.log('token Decoded: ', userDataDecoded);
 
-  obtenerDatosPorUUID(uuid: string) {
-    this.dashboardService.getUserDataById(uuid).subscribe(
-      (response) => {
-        if (response.success && response.message === 'Usuario Encontrado') {
-          console.log('Datos Usuario : ', response.data);
-          const userData = response.data;
-          this.idAccount = userData.idAccount;
-        } else {
-          console.log('Error', 'No se encontró al usuario', 'error');
-        }
-      },
-      (error) => {
-        console.log('Error', 'No se pudieron obtener los datos', 'error');
+    const obtenerDatos = async () => {
+      try {
+        await this.obtenerDatosPorUUID(idUser);
+        await this.obtenerTransaccionesPorAccountId();
+        await this.obtenerDatosCuentaPorAccountId(this.idAccount);
+        await this.parsearData();
+        console.log('La función parsearData se completó.');
+        this.isLoading = false;
+      } catch (error) {
+        console.error('Error:', error);
+        this.isLoading = false;
       }
-    );
+    };
+
+    obtenerDatos();
   }
 
-  async obtenerTransaccionesPorAccountId() {
+  /**
+   * The function `obtenerDatosPorUUID` is an asynchronous function that takes a UUID as a parameter
+   * and returns a Promise that resolves with void.
+   * @param {string} uuid - A string representing the unique identifier of a user.
+   * @returns The function `obtenerDatosPorUUID` returns a Promise of type `void`.
+   */
+  async obtenerDatosPorUUID(uuid: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.dashboardService.getUserDataById(uuid).subscribe(
+        (response) => {
+          if (response.success && response.message === 'Usuario Encontrado') {
+            console.log('Datos Usuario0 : ', response.data);
+            const userData = response.data;
+            this.idAccount = userData.idAccount;
+            resolve();
+          } else {
+            console.log('Error', 'No se encontró al usuario', 'error');
+            reject('No se encontró al usuario'); 
+          }
+        },
+        (error) => {
+          console.error('Error al obtener datos de usuario:', error);
+          reject(error); 
+        }
+      );
+    });
+  }
+
+ /**
+   * The function obtains transactions by account ID and sorts them by date.
+   * @returns a Promise of type void.
+   */
+ async obtenerTransaccionesPorAccountId(): Promise<void> {
+  return new Promise<void>(async (resolve, reject) => {
     try {
       const response = await this.dashboardService.getTransactionsByAccountId(
         this.idAccount
@@ -77,32 +98,74 @@ export class MetricasComponent implements OnInit{
             new Date(b.dateEmit).getTime() - new Date(a.dateEmit).getTime()
           );
         });
+        resolve(); 
       } else {
         console.log('Información', 'No hay transacciones', 'info');
+        reject('No hay transacciones');
       }
     } catch (error) {
-      console.log('Error', 'No se pudieron obtener las transacciones', 'error');
+      console.error('Error al obtener las transacciones', error);
+      reject(error);
     }
-  }
-
-  async obtenerDatosCuentaPorAccountId(idAccount: string) {
-    try {
-      const response = await this.dashboardService.getAccountDataById(
-        idAccount
-      );
-      console.log('Data de la cuenta:', response);
-      this.saldo = response.totalAccount;
-      this.cvu = response.cvu;
-      this.showcvu = true;
-      this.isLoading = false; 
-    } catch (error) {
-      console.error('Error al obtener los datos de la cuenta', error);
-      this.isLoading = false; 
-    }
-  }
-
+  });
+}
   
 
+  /**
+   * The function "obtenerDatosCuentaPorAccountId" retrieves account data by account ID and assigns the
+   * total account balance and CVU to class variables.
+   * @param {string} idAccount - The id of the account for which you want to obtain the data.
+   * @returns a Promise that resolves to void.
+   */
+  async obtenerDatosCuentaPorAccountId(idAccount: string): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+      try {
+        const response = await this.dashboardService.getAccountDataById(
+          idAccount
+        );
+        console.log('Data de la cuenta:', response);
+        this.saldo = response.totalAccount;
+        console.log('Data de la cuenta:', this.saldo);
+        this.cvu = response.cvu;
+        this.showcvu = true;
+        resolve(); 
+      } catch (error) {
+        console.error('Error al obtener los datos de la cuenta', error);
+        reject(error);
+      }
+    });
+  }
+
+  parsearData(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      // Aquí realizarás tus operaciones asincrónicas, como obtener los datos.
+  
+      // Simulemos una operación asincrónica (puedes reemplazar esto con tu lógica real):
+      setTimeout(() => {
+        this.datosTransaccionesSegmentados = this.obtenerSaldosPorTipo();
+  
+        // Verifica si se obtuvieron los datos correctamente
+        if (this.datosTransaccionesSegmentados) {
+          console.log('Montos por tipo (objeto):', this.datosTransaccionesSegmentados);
+  
+          this.doughnutChartLabels = this.datosTransaccionesSegmentados.map((item: any) => item.tipo);
+          console.log('Tipos de transacción:', this.doughnutChartLabels);
+  
+          this.doughnutChartDatasets[0].data = this.datosTransaccionesSegmentados.map((item: any) => item.monto);
+          console.log('Montos para el gráfico:', this.doughnutChartDatasets[0].data);
+  
+          resolve(); // Resuelve la promesa una vez que se han procesado los datos.
+        } else {
+          // En caso de error, puedes rechazar la promesa y proporcionar información adicional sobre el error.
+          reject('No se pudieron obtener los datos');
+        }
+      }, 2000);
+    });
+  }
+  
+
+
+/*
   parsearData(): void {
     setTimeout(() => {
       this.datosTransaccionesSegmentados = this.obtenerSaldosPorTipo();
@@ -128,7 +191,7 @@ export class MetricasComponent implements OnInit{
     }, 2000);
     
     
-  }
+  }*/
 
   showCvu():void{
     this.dashboardService.showCvu();
