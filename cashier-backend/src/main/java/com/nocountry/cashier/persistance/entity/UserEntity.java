de1870d15f32cbec2dd76ed47d0fb62fdb12f493
Raul Gomez <gomez.raul.1978.rg@gmail.com>
2023-10-31 18:01:23 -0300

    fix error mail confirmation

--- cashier-backend/src/main/java/com/nocountry/cashier/persistance/entity/UserEntity.java
+++ cashier-backend/src/main/java/com/nocountry/cashier/persistance/entity/UserEntity.java
@@ -70,7 +70,7 @@ public class UserEntity extends Auditable<LocalDateTime> {
     @JoinColumn(name = "id_token")
     private TokenEntity token;
 
-    @OneToOne(mappedBy = "userEntity")
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "userEntity")
     private AccountEntity accountEntity;
 
     @OneToOne(cascade = {CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE})
