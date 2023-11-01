24beae1dff5af66613bf1c3c70e1b1e3c355fe23
Ra <108808330+Raul1978x@users.noreply.github.com>
2023-11-01 17:22:14 -0300

    Update app-routing.module.ts

--- cashier-app/src/app/app-routing.module.ts
+++ cashier-app/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { authGuard } from './guards/auth.guard';
 import { loginGuard } from './guards/login.guard';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { Found404Component } from './component/found404/found404.component';
-
 
 const routes: Routes = [
   { path: '', redirectTo: 'user', pathMatch: 'full' },
